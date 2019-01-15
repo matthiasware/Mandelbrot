6ea40fce8b6d7769ed893cbd68e6ec232a5658a0
matthias mitterreiter <matthias@mitterreiter.de>
2019-01-15 12:05:39 +0100

    init

---
+++ main.cc
@@ -0,0 +1,12 @@
+#include <QApplication>
+
+#include "mandelbrot_widget.h"
+
+int main(int argc, char** argv){
+	QApplication application(argc,argv);
+
+	MandelbrotWidget widget {};
+	widget.show();
+
+	return application.exec();
+}
---
+++ mandelbrot_widget.h
@@ -0,0 +1,19 @@
+#ifndef MANDELBROT_WIDGET_H_
+#define MANDELBROT_WIDGET_H_ 
+
+#include <QWidget>
+
+class MandelbrotWidget : public QWidget
+{
+	Q_OBJECT
+
+public:
+	MandelbrotWidget(QWidget *parent = nullptr): QWidget(parent) {}
+
+protected:
+
+private:
+
+};
+
+#endif // MANDELBROT_WIDGET_H_
