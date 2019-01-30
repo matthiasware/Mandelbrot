ad51517beed6c8b2f4245d63fc679566e89c2676
matthias mitterreiter <matthias@mitterreiter.de>
2019-01-30 01:15:14 +0100

    command line arguments

--- bmpcreator.cc
+++ bmpcreator.cc
@@ -5,6 +5,7 @@
 #include <iomanip>
 #include <math.h>
 #include <cstring>
+#include <stdlib.h>
 //  https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=AVX,AVX2&cats=Store&expand=3525
 
 void inspect(__m256d &v)
@@ -35,11 +36,82 @@ void inspect(__m128i &v)
   std::cout << d[0] << " " << d[1]  << " " << d[2] << " " << d[3] << std::endl;
 }
 
-// w, h, maxiter, name
+/*
+--width int
+--height int
+--maxiter int
+--name str
+--imin double
+--imax double
+--rmin double
+--rmax double
+*/
 int main(int argc, char** argv) {
 
-  for (int i = 0; i < argc; ++i) 
-    std::cout << argv[i] << "\n"; 
+  // default values
+  int w = 10;
+  int h = 20;
+  int maxiter = 30;
+  double re_min = 40;
+  double re_max = 50;
+  double im_min = 60;
+  double im_max = 70;
+  std::string name = "mandelbrot";
+
+  std::string usage =  "Usage: ./bmcreator\n"
+                       "\t --name <outfile>\n"
+                       "\t --width <pixel_width>\n"
+                       "\t --height <pixel_height>\n"
+                       "\t --maxiter <maxiter>\n"
+                       "\t --imin <minimum imaginary value>\n"
+                       "\t --imax <maximum imaginary value>\n"
+                       "\t --rmin <minimum real value>\n"
+                       "\t --rmax <maximum real value>\n";
+  if((argc -1)%2 != 0)
+  {
+    std::cout << usage;
+    return 0;
+  }
+  for (int i = 1; i < argc; i+=2)
+  {
+    if(argv[i] == std::string("--width"))
+    {
+      w = atoi(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--height"))
+    {
+      h = atoi(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--name"))
+    {
+      name = std::string(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--maxiter"))
+    {
+      maxiter = atoi(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--imin"))
+    {
+      im_min = atof(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--imax"))
+    {
+      im_max = atof(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--rmin"))
+    {
+      re_min = atof(argv[i+1]);
+    }
+    else if(argv[i] == std::string("--rmax"))
+    {
+      re_max = atof(argv[i+1]);
+    }
+    else
+    {
+      std::cout << usage;
+      return 0;
+    }
+  }
   // int w = 40;
   // int h = 10;
   // int maxiter = 1000;
